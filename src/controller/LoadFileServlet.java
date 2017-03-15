7403478f3c4275444f1f6ac75d1d97836ac1ed9c
Vladislav Osipenkov <JumpViaOcean@gmail.com>
2017-03-15 10:53:39 +0400

    sceletons for delete, add, download

---
+++ src/controller/LoadFileServlet.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by 21cmPC on 15.03.2017.
+ */
+public class LoadFileServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("text/html");
+        out.print("Load page");
+        out.flush();
+    }
+}
--- src/controller/PreprocessingServlet.java
+++ src/controller/PreprocessingServlet.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by 21cmPC on 15.03.2017.
@@ -14,12 +13,23 @@ public class PreprocessingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
 
-        out.println(req.getParameter("file"));
-        out.println("<br>");
-        out.print(req.getParameter("action"));
+        String fileName = req.getParameter("file");
+        String action = req.getParameter("action");
 
-        out.flush();
+        this.getServletContext().setAttribute("fileName", fileName);
+
+        switch (action) {
+            case "delete":
+                resp.sendRedirect("http://localhost:8080/delete");
+                break;
+            case "download":
+                resp.sendRedirect("http://localhost:8080/download");
+                break;
+            case "load":
+                resp.sendRedirect("http://localhost:8080/load");
+            default:
+                break;
+        }
     }
 }
